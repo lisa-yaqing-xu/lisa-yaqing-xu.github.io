6bd8cd1bd258dad1751f9cdff0e35e21e4cbe8b3
Lisa Xu <lisa.yaqing.xu@gmail.com>
2023-06-28 10:08:19 -0400

    set up base hamburger menu logic

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,18 +1,45 @@
-import { Component, ElementRef, HostBinding, Input } from '@angular/core';
+import { Component, ElementRef, HostBinding, HostListener, Input } from '@angular/core';
 import { RouteConfig } from 'src/app/config/routes';
 import { IRouteConfigItem } from 'src/app/interfaces/route-config.interface';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss', './header-collapsed.component.scss']
 })
-export class HeaderComponent{
+export class HeaderComponent {
+  @Input() @HostBinding('class.lx-header--collapsed') set menuCollapsed(collapsed: boolean) {
+    this._menuCollapsed = collapsed;
+    if(!collapsed){
+      this.hamburgerActive = false;
+    }
+  };
+  get menuCollapsed() {
+    return this._menuCollapsed;
+  }
+  readonly smallScreenCutoff = 400;
   @HostBinding('class.lx-header') baseClass = true;
-  @Input() @HostBinding('class.lx-header--collapsed') menuCollapsed = false;
+  @HostBinding('class.lx-header--small') smallScreen = window.innerWidth <= this.smallScreenCutoff;
   routes: IRouteConfigItem[] = Object.values(RouteConfig);
 
-  constructor(private element: ElementRef) {}
+  hamburgerActive = false;
 
+  private _menuCollapsed = false;
 
+  @HostListener('window:resize') onResize() {
+    this.smallScreen = window.innerWidth <= this.smallScreenCutoff;
+    if (!this.smallScreen) {
+      this.hamburgerActive = false;
+    }
+  }
+
+  constructor(private element: ElementRef) { }
+
+  openHamburger() {
+    this.hamburgerActive = true;
+  }
+
+  closeHamburger(){
+    this.hamburgerActive = false;
+  }
 }
