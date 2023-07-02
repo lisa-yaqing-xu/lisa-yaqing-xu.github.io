ba2b488e0d9aaca253c66cec2af83b634011b9df
Lisa Xu <lisa.yaqing.xu@gmail.com>
2023-07-02 17:11:43 -0400

    favicon fix

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 ng build --configuration production
 mkdir -p docs || exit 1
 cp -a dist/lisa-personal-site/. docs
+cp src/favicon.png docs/favicon.png
 cp CNAME docs/CNAME
 cp robots.txt docs/robots.txt
 cp docs/index.html docs/404.html
