81e7c7316fdf337d91b751439f22f7f8d7d20457
Lisa Xu <lisa.yaqing.xu@gmail.com>
2023-07-02 00:15:08 -0400

    added service to handle focus traps

--- src/app/components/gallery-overlay/gallery-overlay.component.ts
+++ src/app/components/gallery-overlay/gallery-overlay.component.ts
@@ -1,14 +1,14 @@
-import { Component, EventEmitter, HostBinding, HostListener, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, HostBinding, HostListener, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChanges } from '@angular/core';
 import { IGalleryItem } from 'src/app/interfaces/gallery.interface';
 import { IOverlay } from 'src/app/interfaces/overlay.interface';
-import { OverlayHandlerService } from 'src/app/services/overlay-handler.service';
+import { OverlayHandler, OverlayHandlerService } from 'src/app/services/overlay-handler.service';
 
 @Component({
   selector: 'app-gallery-overlay',
   templateUrl: './gallery-overlay.component.html',
   styleUrls: ['./gallery-overlay.component.scss'],
 })
-export class GalleryOverlayComponent implements OnInit, OnDestroy {
+export class GalleryOverlayComponent implements OnInit, OnDestroy, OnChanges {
   @Input() currentItem: IGalleryItem;
   @Input() hasPrevious = false;
   @Input() hasNext = false;
@@ -33,12 +33,21 @@ export class GalleryOverlayComponent implements OnInit, OnDestroy {
     }
   }
 
-  private overlayData: IOverlay;
+  private overlayData: OverlayHandler;
 
-  constructor(private overlay: OverlayHandlerService){}
+  constructor(private element: ElementRef, private overlay: OverlayHandlerService) { }
 
   ngOnInit(): void {
-    this.overlayData = this.overlay.setup();
+    this.overlayData = this.overlay.setup({
+      baseElement: this.element,
+      focusTrapElements: [this.element]
+    });
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['hasPrevious'] || changes['hasNext']) {
+      setTimeout(() => { this.overlayData?.updateFocusableElements(); }, 0);
+    }
   }
 
   ngOnDestroy(): void {
--- src/app/services/overlay-handler.service.ts
+++ src/app/services/overlay-handler.service.ts
@@ -1,18 +1,120 @@
-import { Injectable } from '@angular/core';
-import { IOverlay } from '../interfaces/overlay.interface';
-
+import { ElementRef, Injectable } from '@angular/core';
+import { IOverlay, IOverlayInitData } from '../interfaces/overlay.interface';
 @Injectable({
   providedIn: 'root'
 })
 export class OverlayHandlerService {
-
   constructor() { }
 
-  setup(): IOverlay {
+  setup(data: IOverlayInitData): OverlayHandler {
+    const overlayHandler = new OverlayHandler(data);
+    return overlayHandler;
+  }
+}
+
+
+export class OverlayHandler {
+  // https://stackoverflow.com/questions/1599660/which-html-elements-can-receive-focus
+  // this is a watered down version of the string since this is a personal site and I'm not out here
+  // writing forms here, so it really just boils down to buttons, links, and tabindex.
+  readonly focusableElementString = `a[href], button:not([disabled]), [tabindex='0']`;
+  private focusableElements: HTMLElement[];
+  private focusableElementSet: Set<HTMLElement>;
+  private eventCache: { tabEvent?: () => void } = { tabEvent: null };
+  private baseElement: ElementRef;
+  private focusTrapParents: ElementRef[];
+  private defaultFocusElement: HTMLElement;
+
+  constructor(private data: IOverlayInitData) {
+    this.baseElement = data.baseElement;
+    this.focusTrapParents = data.focusTrapElements;
+    this.init();
+  }
+
+
+
+  init() {
     document.body.classList.add('overlay-visible');
-    const teardown = () => {
-      document.body.classList.remove('overlay-visible');
-    };
-    return { teardown };
+    this.setFocusableElements();
+    const tentativeDefault = this.getTentativeDefaultFocusElement();
+    this.setDefaultFocusElement(tentativeDefault);
+    this.defaultFocusElement.focus();
+    this.eventCache.tabEvent = this.onTab.bind(this);
+    this.baseElement.nativeElement.addEventListener('keydown', this.eventCache.tabEvent);
+  }
+
+  updateFocusableElements() {
+    this.setFocusableElements();
+    this.setDefaultFocusElement(this.defaultFocusElement);
+    if (!this.hasLastFocusElement()) {
+      this.defaultFocusElement.focus();
+    }
+  }
+
+  onTab(event: KeyboardEvent) {
+    if (event.key !== 'Tab') {
+      return;
+    }
+    let updateFocusElement;
+    if (!this.focusableElements.length) {
+      updateFocusElement = this.defaultFocusElement;
+    } else if (this.hasLastFocusElement()) {
+      const first = this.focusableElements[0];
+      const last = this.focusableElements[this.focusableElements.length - 1];
+      if (event.shiftKey && document.activeElement === first) {
+        updateFocusElement = last;
+      } else if (!event.shiftKey && document.activeElement === last) {
+        updateFocusElement = first;
+      }
+    }
+    if (updateFocusElement) {
+      event.preventDefault();
+      setTimeout(()=>{
+        updateFocusElement.focus();
+      },0);
+    }
+  }
+
+  teardown() {
+    document.body.classList.remove('overlay-visible');
+    this.baseElement.nativeElement.removeEventListener('keydown', this.eventCache.tabEvent);
+  }
+
+  private getTentativeDefaultFocusElement() {
+    let defaultFocusElement;
+    if (this.data.focusElement) {
+      defaultFocusElement = this.data.focusElement;
+    } else if (this.data.focusElementSelector) {
+      defaultFocusElement = this.data.baseElement.nativeElement.querySelector(this.data.focusElementSelector)
+        || this.baseElement.nativeElement;
+    } else {
+      defaultFocusElement = this.baseElement.nativeElement;
+    }
+    return defaultFocusElement;
+  }
+
+  private setFocusableElements() {
+    this.focusableElements = this.focusTrapParents.reduce((total, element) => {
+      if (element.nativeElement.matches(this.focusableElementString)) {
+        total.push(element.nativeElement); // count in focusable self
+      }
+      const focusableChildren = Array.from(element.nativeElement.querySelectorAll(this.focusableElementString));
+      return total.concat(focusableChildren);
+    }, []);
+    this.focusableElementSet = new Set(this.focusableElements);
+  }
+
+  private setDefaultFocusElement(tentativeDefault: HTMLElement) {
+    if (tentativeDefault.matches(this.focusableElementString)) {
+      this.defaultFocusElement = tentativeDefault;
+    } else if (this.focusableElements.length) {
+      this.defaultFocusElement = this.focusableElements[0]
+    } else {
+      throw new Error('no focusable elements within this modal!');
+    }
+  }
+
+  private hasLastFocusElement() {
+    return this.focusableElementSet.has(document.activeElement as HTMLElement);
   }
 }
