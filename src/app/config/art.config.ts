b8a3ebc482cbb617e61b4639e042ef5b69e0e8f5
Lisa Xu <lisa.yaqing.xu@gmail.com>
2023-07-02 18:55:22 -0400

    illustration descriptions done

--- src/app/config/art.config.ts
+++ src/app/config/art.config.ts
@@ -9,61 +9,93 @@ export const LXGallery: IGallery[] = [
                 title: 'Meteor Strike',
                 name: 'illustration-meteor-strike',
                 altText: '',
-                url: 'assets/art/draw_ayra.png'
+                year: 2016,
+                url: 'assets/art/draw_ayra.png',
+                description: `Ayra is my favorite character from 1996 SNES game Fire Emblem: Geneology of the Holy War--And really, my favorite character in the entire Fire Emblem series.
+                Her family has a powerful secret sword skill named Meteor Sword (localized as Astra in more recent entries) that strikes the enemy 5 times at extremely high speeds. I painted her executing this technique on a generic enemy here.`
             },
             {
                 title: 'Death From Above',
                 name: 'illustration-death-from-above',
                 altText: '',
-                url: 'assets/art/draw_edward.png'
+                year: 2013,
+                url: 'assets/art/draw_edward.png',
+                description: `This is my entry for an official art competition from Ubisoft back when Assassin's Creed: Black Flag came out in 2013.
+                I made it to the finalists.`
+
             },
             {
                 title: '24 Hour Judgement',
                 name: 'illustration-24-hr-judgement',
                 altText: '',
-                url: 'assets/art/draw_24hrjudgement.png'
+                url: 'assets/art/draw_24hrjudgement.png',
+                year: 2022,
+                description: `I played Yakuza 0 (2015) and it became my favorite game of all time. Kiryu and Majima both are now some of my absolute favorite characters in fiction. 
+                In the game you can bring the protagonists to sing Karaoke, which is implemented as a rhythm minigame. If you do well enough in the minigame, the game will play an accompanying cinematic, and sometimes the characters wear different costumes in them.
+                Here, Kiryu is depicted in his costume from Judgement ~Shinpan~ and Majima is depicted in his costume for 24 Hour Cinderella, their respective main song in Yakuza 0.`
             },
             {
                 title: 'Shannan\'s Training',
                 name: 'illustration-shannans-training',
                 altText: '',
-                url: 'assets/art/draw_ayrashannan.png'
+                url: 'assets/art/draw_ayrashannan.png',
+                year: 2020,
+                description: `In Fire Emblem: Geneology of the Holy War, Ayra's home country was conquered and she was tasked with protecting her brother's son, Shannan, as they fled the country.
+                The player recruits her into the army after saving Shannan fron the people holding him hostage to force Ayra to fight for them. We meet Shannan as a child, at the time.
+                17 years later, Shannan has grown into a formidable fighter on his own, and it's thanks to Ayra's training.`
+
             },
             {
                 title: 'Dark Blade of Od',
                 name: 'illustration-dark-blade-of-od',
                 altText: '',
-                url: 'assets/art/draw_galzus.png'
+                url: 'assets/art/draw_galzus.png',
+                year: 2017,
+                description: `Galzus is from 1999 SNES game Fire Emblem: Thracia 776. He's a mercenary who lost his homeland and had to flee with his family. On the road, his daughter was stolen from him, and he fights to survive with the only hope that his daughter one day reunites with him.
+                She does, eventually.`
             },
             {
                 title: 'Hell Stew Eagles',
                 name: 'illustration-hell-stew-eagles',
                 altText: '',
-                url: 'assets/art/draw_hellstewbert.png'
+                url: 'assets/art/draw_hellstewbert.png',
+                year: 2023,
+                description: `In Fire Emblem 3 Houses, one of my favorite characters, Hubert von Vestra, is voiced by Robbie Daymond. In Yakuza: Like a Dragon, one of my favorite characters, Zhao Tianyou, is also voiced by Robbie Daymond. Hell Stew is Zhao's karaoke song.
+                So I had to draw Hubert and his friends performing it. It really fits his vibe, too.`
             },
             {
                 title: 'Carefree Saint',
                 name: 'illustration-carefree-saint',
                 altText: '',
-                url: 'assets/art/draw_laura.png'
+                year: 2021,
+                url: 'assets/art/draw_laura.png',
+                description: `Laura is a cleric from 2007 Wii game Fire Emblem: Radiant Dawn. She's one of the favorite characters of one of my good friends. I got him for a secret santa and thought it would be fun to do a portrait of her while trying to replicate the feel of oil on canvas.`
             },
             {
                 title: 'Njörun',
                 name: 'illustration-njorun',
                 altText: '',
-                url: 'assets/art/draw_njorun.png'
+                url: 'assets/art/draw_njorun.png',
+                year: 2020,
+                description: `In Fire Emblem: Geneology of the Holy War, there are 12 Heroes that sealed away the dark dragon 120 years prior to the start of the game. Njörun was one of the 12, and is the ancestor to a few major characters in the playable cast.`
             },
             {
                 title: 'Beyond the River',
                 name: 'illustration-beyond-the-river',
                 altText: '',
-                url: 'assets/art/draw_reinolwen.png'
+                url: 'assets/art/draw_reinolwen.png',
+                year: 2019,
+                description: `Chapter 22 of Fire Emblem: Thracia 776 is notorious for its difficulty, one aspect of which is its tough mid-boss Reinhardt. However, if you have Reinhardt's sister, Olwen, in your army, she can talk to him--and while he won't stand down, as he doesn't want to go against his liege, he gives her his prized sword which, mechanically, makes fighting against him much easier.
+                One interpretation--one that I hold, is that while he refuses to go against his boss, he knew deep down that Olwen's cause is the right one.`
             },
             {
                 title: 'Two Sword Princes',
                 name: 'illustration-two-sword-princes',
                 altText: '',
-                url: 'assets/art/draw_ryoshan.png'
+                year: 2018,
+                url: 'assets/art/draw_ryoshan.png',
+                description: `There's a lot of similarities to Shannan (Fire Emblem: Geneology of the Holy War) and Ryoma (Fire Emblem Fates: Birthright). They're princes of their respective countries--and become kings at the end of the game. They are considered some of the most skilled swordsmen in their respective games. They have legendary swords. They're father figures to their younger same-generation family members. They have long hair.
+                They also have crippling guilt from an event they were unable to prevent as children. If they'd ever meet I think they'd have a lot to talk about.`
             }
         ]
     },
@@ -75,7 +107,9 @@ export const LXGallery: IGallery[] = [
                 title: 'Glamrock Meteor',
                 name: 'illustration-glamrock-meteor',
                 altText: '',
-                url: 'assets/art/design_glamayra.png'
+                url: 'assets/art/design_glamayra.png',
+                year: 2018,
+                description: ``
             },
             {
                 title: 'Character Design 1',
